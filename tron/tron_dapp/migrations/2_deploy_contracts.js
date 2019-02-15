1e3c2570f02e4b4be87059caa65d545941b2cb52
yanwankunde@qq.com <yanwankunde@qq.com>
2019-02-15 13:24:02 +0800

    aaa

--- tron/tron_dapp/migrations/2_deploy_contracts.js
+++ tron/tron_dapp/migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-// var MyContract = artifacts.require("./MyContract.sol");
+var GameDiceToken = artifacts.require("./GameDiceToken.sol");
 
 module.exports = function(deployer) {
-  // deployer.deploy(MyContract);
+    deployer.deploy(GameDiceToken, 10000000000, "YWKTS", 5, "WK");
 };
